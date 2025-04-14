849e207ad22ab3d20515a9cf2d3d7e9cc1e909c5
start.vaadin.com <no-reply@vaadin.com>
2025-04-14 17:26:07 +0000

    Generated project

---
+++ src/main/java/net/dancer/jump/base/domain/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains reusable domain classes.
+ */
+@NullMarked
+package net.dancer.jump.base.domain;
+
+import org.jspecify.annotations.NullMarked;
---
+++ src/main/java/net/dancer/jump/base/ui/component/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains reusable UI components.
+ */
+@NullMarked
+package net.dancer.jump.base.ui.component;
+
+import org.jspecify.annotations.NullMarked;
